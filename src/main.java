fd1585b5b933a55efdccd84a3a3d9bfb9e07db76
Michael Brave <michaelbrave@gmail.com>
2021-01-25 05:18:54 -0600

    this assignment was too vague

--- src/main.java
+++ src/main.java
@@ -1,6 +1,14 @@
+import java.sql.SQLOutput;
+
 public class main {
-    private static int myInt = 4;
+    private static final int myInt = 4;
     //System.out.printf("hello %s", myInt); // this should not work
     System.out.printf("hello %d", myInt);
+}
+
+// this won't work
+/*
+main {
 
 }
+ */
